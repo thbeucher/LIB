028e6a7c9dc3345a473e3694be34e4949264775e
thbeucher <beucherlamacque.thomas@gmail.com>
2016-03-21 12:05:59 +0100

    Utils modified, readParams function added

--- Utils.py
+++ Utils.py
@@ -10,6 +10,7 @@
 #-------------------------------------------------------------------------------
 
 import shutil
+import numpy as np
 
 def zipData(zipFileName, pathToZip):
     '''
@@ -54,3 +55,28 @@ def clamp(var, mini, maxi):
     if var > maxi:
         var = maxi
     return var
+
+def readParams(pathToParam):
+    '''
+    Reads a parameters file and set data into a python dictionary
+
+    Input:
+        pathToParam - String - path to the parameters file
+
+    Output:
+        p - python dictionary - key = name of the parameter, value = value of the parameter
+    '''
+    with open(pathToParam, 'r') as file:
+        alls = file.readlines()
+
+    p = {}
+    for line in alls:
+        a = line.split(":")
+        if a[0] == "nonbhl":
+            t = a[1].split(",")
+            t = [int(el) for el in t]
+            p[a[0]] = np.asarray(t)
+        else:
+            p[a[0]] = float(a[1])
+
+    return p
